5e34ba8797ea50e065c6979f1c07f3ac3bc458f0
Arshit Singh <arshit.singh@accenture.com>
2024-06-18 17:03:38 +0530

    Product Container Changes

---
+++ src/app/productlist/productlist.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productlist',
+  templateUrl: './productlist.component.html',
+  styleUrls: ['./productlist.component.css']
+})
+export class ProductlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+//name ='arshit'
+addToCart:number=0;
+  product = {
+    name : 'iPhone 15',
+    price : 789,
+    color : 'Blue',
+    discount : 8.5,
+    inStock : 1,
+    pImage : '/assets/images/iphone.jpg'
+  }  
+  getDiscountedPrice(){
+    return this.product.price - (this.product.price * this.product.discount/100 )
+  }
+  onNameChange(event:any){
+    //this.name=event.target.value;
+  }
+  decrementCartValue(){
+    if(this.addToCart >0){
+      this.addToCart--;
+    }    
+  }
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
+    
+  }
+}
